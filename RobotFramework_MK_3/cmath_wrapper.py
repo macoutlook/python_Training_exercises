818cbde1d57f16909e0d33fab0403d7a05add4b5
lozoxmac <maciej.lozowski@tieto.com>
2016-02-23 13:30:46 +0100

    Documentation for RobotFramework_MK_3

--- RobotFramework_MK_3/cmath_wrapper.py
+++ RobotFramework_MK_3/cmath_wrapper.py
@@ -8,37 +8,40 @@ cmath_tests.tsv contains tests which can be executed in RobotFramework: pybot cm
 
 def get_log10(x):
     """
-    wrapper method for cmath.log10(x) function, calculates log10 for complex number
-    :param x: complex number
-    :return: log 10 of complex number
+    Wrapper method for cmath.log10(x) function, calculates log10 for complex number
+
+    Examples:
+    | Get log10 | 100 + 0j |
     """
     return cmath.log10(complex(x))
 
 
 def get_sin(x):
     """
-    wrapper method for cmath.sin(x) function, calculates sinus for int number. Can be implemented for complex number too
-    :param x: int number
-    :return: sinus for int number x
+    Wrapper method for cmath.sin(x) function, calculates sinus for int number. Can be implemented for complex number too
+
+    Examples:
+    | Get Sin | 30 |
     """
     return cmath.sin(int(x))
 
 
 def get_sqrt(x):
     """
-    wrapper method for cmath.sqrt(x) function, calculates sqrt for complex number
-    :param x: complex number
-    :return: sqrt of complex number
+    Wrapper method for cmath.sqrt(x) function, calculates sqrt for complex number
+
+    Examples:
+    | Get Sqrt | 16 + 16j |
     """
     return cmath.sqrt(complex(x))
 
 
 def check_sqrt(x, res):
     """
-    method which checks, whether given result of sqrt equals sqrt of cmath.sqrt method.
-    :param x: complex number for which sqrt is calculated
-    :param res: result which should be equaled to calculated sqrt of x
-    :return: raises assertion error, if res is not equaled to x
+    Method which checks, whether given result of sqrt equals sqrt of cmath.sqrt method.
+
+    Examples:
+    | Check Sqrt | 4 + 0j | 16 + 0j |
     """
     if cmath.sqrt(complex(x)) != complex(res):
         AssertionError("Given result is not proper")
